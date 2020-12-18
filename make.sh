656fe7d9bb8a9a29b2baf054989bde1ec521ff42
animesh.nepal <st163747@stud.uni-stuttgart.de>
2020-12-19 00:05:07 +0100

    Initial commit

---
+++ make.sh
@@ -0,0 +1,13 @@
+pdflatex main.tex
+bibtex main.aux
+pdflatex main.tex
+pdflatex main.tex
+rm -rf \
+ main.aux \
+ main.bbl \
+ main.blg \
+ main.lof \
+ main.log \
+ main.lot \
+ main.out \
+ main.toc 
